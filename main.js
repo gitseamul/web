37c11c76eead3e7d4c731b7e4d83e3110c47768d
gitseamul <scvscsc@gmail.com>
2020-06-24 14:03:03 +0900

    bugfix

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('.navbar__menu');
 const icons = document.querySelector('.navbar__icons');
 
 toggleBtn.addEventListener('click', ()=> {
-  menu.classList.toggle('avtive');
-  icons.classList.toggle('avtive');
+  menu.classList.toggle('active');
+  icons.classList.toggle('active');
 
 });
